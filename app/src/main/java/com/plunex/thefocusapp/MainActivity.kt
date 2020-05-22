a1965d5efe6d3b1f9f70ba196c2c7ce38f8fb66c
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2020-05-22 20:34:32 +0100

    First Commit

---
+++ app/src/main/java/com/plunex/thefocusapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.plunex.thefocusapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_set_focus -> {
+                startActivity(Intent(this, SetFocusActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/plunex/thefocusapp/SetFocusActivity.kt
@@ -0,0 +1,12 @@
+package com.plunex.thefocusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetFocusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ic_focus)
+    }
+}
